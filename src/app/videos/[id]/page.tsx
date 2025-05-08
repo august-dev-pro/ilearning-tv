2eef98b3e1b0c9429f2f177c2bab03593d460c6a
august-dev-pro <augustinselete01@gmail.com>
2025-05-08 18:05:46 +0000

    fix: corrected page props type

--- src/app/videos/[id]/page.tsx
+++ src/app/videos/[id]/page.tsx
@@ -1,11 +1,14 @@
 import VideoCard from "@/components/videos/VideoCard2";
 import { Video, videos } from "@/lib/api/videosData";
-type PageProps = {
-  params: { id: string };
-};
 
-export default async function VideoPageById({ params }: PageProps) {
-  const { id } = params;
+interface Props {
+  params: { id: string };
+}
+
+export default async function VideoPageById({ params }: Props) {
+  const { id } = await params;
+  console.log(id);
+
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
       <main className="max-w-6xl mx-auto px-4 py-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
